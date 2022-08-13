373e518eaa63f52e78ce5f390100148bcaafc448
Minh Nguyen <shenlong2361996@gmail.com>
2022-08-13 23:59:29 +0300

    add info container when being hovered in each project tile

--- components/Project/Card.vue
+++ components/Project/Card.vue
@@ -3,6 +3,10 @@
     <div class="profile-picture">
       <img :src="project.imgSrc" alt="">
     </div>
+    <div class="overlay">
+      <h2>{{ project.title }}</h2>
+      <p>{{ project.description }}</p>
+    </div>
   </b-container>
 </template>
 
@@ -21,6 +25,8 @@ export default {
 .project-card{
   width: 28rem;
 
+  position: relative;
+
   // margin: 0 !important;
   padding: 0 !important;
 
@@ -28,12 +34,51 @@ export default {
 
   flex-basis:auto;
 
+  &:hover{
+    .overlay{
+      display: block;
+      // transition: transform 2s;
+      // transform: translateY(-1rem);
+    }
+  }
+
   .profile-picture{
     width: 100%;
+
+    z-index: 1;
 
     img{
       max-width: 100%;
       width: 28rem;
+    }
+  }
+
+  .overlay{
+    z-index: 4;
+    position: absolute;
+    bottom:0;
+
+    display: none;
+
+    height: 33%;
+    width: 100%;
+
+    background: linear-gradient(180deg, rgba(0, 0, 0, 0.6) 0%, rgba(255, 255, 255, 0) 100%);
+
+    padding: 1rem 1.5rem;
+
+    text-transform: capitalize;
+
+    // transform: translateY(9rem);
+    h2{
+      color: $button-text-color;
+
+      margin-bottom: 1rem;
+    }
+
+    p{
+      color: $button-text-color;
+      font-size: 1.625rem;
     }
   }
 }
