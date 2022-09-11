61c1b726ed13df500090bd946fe1c766e20574f3
Minh Nguyen <shenlong2361996@gmail.com>
2022-09-11 20:49:29 +0300

    add hover effect for project card, overlay and image

--- components/Project/Card.vue
+++ components/Project/Card.vue
@@ -2,7 +2,9 @@
   <b-container fluid="md" class="project-card">
     <div class="profile-picture">
       <img :src="project.imgSrc" alt="">
+      <div class="hover-background" />
     </div>
+
     <div class="overlay">
       <h2>{{ project.title }}</h2>
       <p>{{ project.description }}</p>
@@ -22,34 +24,68 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+/**
+  KEYFRAMES
+*/
+@keyframes hoverImageZoom {
+  from{
+        transform: scale(1);
+  }
+  to{
+    transform: scale(1.5);
+  }
+}
+
 .project-card{
   width: 28rem;
 
   position: relative;
 
-  // margin: 0 !important;
   padding: 0 !important;
 
   margin: 0 0 1rem 0 !important;
 
   flex-basis:auto;
 
+  overflow: hidden;
+
+  cursor: pointer;
+
   &:hover{
+    .profile-picture{
+      img{
+        transform: scale(1.5);
+      }
+
+      .hover-background{
+        background: rgba(0, 0, 0, 0.6);
+
+        position: absolute;
+        top: 0;
+        width:100%;
+        height:100%;
+      }
+    }
+
     .overlay{
-      display: block;
-      // transition: transform 2s;
-      // transform: translateY(-1rem);
+      transform: translateY(-1rem);
     }
   }
 
   .profile-picture{
     width: 100%;
 
+    position: relative;
     z-index: 1;
+
+    overflow: hidden;
 
     img{
       max-width: 100%;
       width: 28rem;
+
+      transition: all .2s;
     }
   }
 
@@ -57,8 +93,6 @@ export default {
     z-index: 4;
     position: absolute;
     bottom:0;
-
-    display: none;
 
     height: 33%;
     width: 100%;
@@ -69,10 +103,11 @@ export default {
 
     text-transform: capitalize;
 
-    // transform: translateY(9rem);
+    transform: translateY(9.2rem);
+    transition: transform .1s ease-in-out;
+
     h2{
       color: $button-text-color;
-
       margin-bottom: 1rem;
     }
 
